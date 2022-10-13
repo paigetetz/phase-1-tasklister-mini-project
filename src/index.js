2a2e5fd65cff7d151960df72e015aee5e47253f4
paige <paigetetz@gmail.com>
2022-10-13 12:31:07 -0400

    - m 'lots of edits'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let taskForm = document.getElementById('create-task-form')
+    taskForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      newTask (e.target['new-task-description'].value)
+    })
 });
+
+function newTask(todo){
+  let listItem = document.createElement('p')
+  let tasks = document.getElementById('tasks')
+  listItem.textContent = (todo)
+  tasks.appendChild(listItem)
+}
